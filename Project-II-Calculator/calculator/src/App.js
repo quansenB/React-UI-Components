ca9665b3318ef603e51aa4497a7f068aaaaabcb8
quansenB <inaki.breinbauer@gmail.com>
2019-05-29 00:00:36 +0200

    modify comonents structure

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -4,33 +4,46 @@ import './App.css';
 const App = () => {
   return (
     <div className="container">
-      <Field className="4 black 0"width="4" color="black" content="0"/>
-      <Field className="3 white clear"width="3" color="white" content="clear"/>
-      <Field classname="1 red /"width="1" color="red" content="/"/>
-      <Field width="1" color="white" content="7"/>
-      <Field width="1" color="white" content="8"/>
-      <Field width="1" color="white" content="9"/>
-      <Field width="1" color="red" content="*"/>
-      <Field width="1" color="white" content="4"/>
-      <Field width="1" color="white" content="5"/>
-      <Field width="1" color="white" content="4"/>
-      <Field width="1" color="red" content="-"/>
-      <Field width="1" color="white" content="4"/>
-      <Field width="1" color="white" content="4"/>
-      <Field width="1" color="white" content="4"/>
-      <Field width="1" color="red" content="+"/>
-      <Field width="2" color="white" content="4"/>
-      <Field width="1" color="red" content="="/>
+      <Field4 className="4 black 0" width="4" color="black" content="0"/>
+      <Field3 className="3 white clear" width="3" color="white" content="clear"/>
+      <Field1 classname="1 red /" width="1" color="red" content="/"/>
+      <Field1 width="1" color="white" content="7"/>
+      <Field1 width="1" color="white" content="8"/>
+      <Field1 width="1" color="white" content="9"/>
+      <Field1 width="1" color="red" content="*"/>
+      <Field1 width="1" color="white" content="4"/>
+      <Field1 width="1" color="white" content="5"/>
+      <Field1 width="1" color="white" content="6"/>
+      <Field1 width="1" color="red" content="-"/>
+      <Field1 width="1" color="white" content="1"/>
+      <Field1 width="1" color="white" content="2"/>
+      <Field1 width="1" color="white" content="3"/>
+      <Field1 width="1" color="red" content="+"/>
+      <Field3 width="3" color="white" content="0"/>
+      <Field1 width="1" color="red" content="="/>
     </div>
   );
 };
 
-function Field(width, color, content){
-  const style ={
-    style.width : (25*width)%
-    style.color: color;
-  }
-  return <div>{content}</div>
+function Field1(props){
+  
+  return <div>
+  {props.content}
+  </div>
+}
+
+function Field3(props){
+  
+  return <div>
+  {props.content}
+  </div>
+}
+
+function Field4(props){
+  
+  return <div>
+  {props.content}
+  </div>
 }
 
 export default App;
